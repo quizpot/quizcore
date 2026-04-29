f96b2ff921c216de0682c784197748d31e1d19f8
kragleh <kragleh@gmail.com>
2026-04-29 15:51:33 +0200

    Fix State Definition

--- src/types/lobby/player-lobby-state.ts
+++ src/types/lobby/player-lobby-state.ts
@@ -1,8 +1,7 @@
 import z from "zod";
 import { LobbyStatusSchema, PlayerSchema } from "./lobby";
 import { QuizInfoSchema } from "../quiz/quiz";
-import { SafeQuestionSchema } from "../quiz/safe-question";
-import { QuizStepSchema } from "../quiz/step";
+import { SafeQuizStepSchema } from "../quiz/step";
 
 export const PlayerLobbyStateSchema = z.object({
   code: z.string(),
@@ -11,7 +10,7 @@ export const PlayerLobbyStateSchema = z.object({
   hostConnected: z.boolean(),
   stepNumber: z.number().int().nonnegative(),
   quizInfo: QuizInfoSchema,
-  currentStep: z.optional(QuizStepSchema),
+  currentStep: z.optional(SafeQuizStepSchema),
   timeout: z.optional(z.iso.datetime()),
   hasAnswered: z.boolean(),
   wasCorrect: z.boolean(),
--- src/types/quiz/step.ts
+++ src/types/quiz/step.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import { Question, QuestionSchema } from "./question";
 import { SlideLayout, SlideLayoutSchema } from "./slide";
+import { SafeQuestion, SafeQuestionSchema } from "./safe-question";
 
 export const QuizStepSchema = z.discriminatedUnion("type", [
   z.object({ type: z.literal("question"), data: QuestionSchema }),
@@ -11,6 +12,21 @@ export type QuizStep =
   | {
       type: "question";
       data: Question;
+    }
+  | {
+      type: "slide";
+      data: SlideLayout;
+    };
+
+export const SafeQuizStepSchema = z.discriminatedUnion("type", [
+  z.object({ type: z.literal("question"), data: SafeQuestionSchema }),
+  z.object({ type: z.literal("slide"), data: SlideLayoutSchema }),
+]);
+
+export type SafeQuizStep =
+  | {
+      type: "question";
+      data: SafeQuestion;
     }
   | {
       type: "slide";
